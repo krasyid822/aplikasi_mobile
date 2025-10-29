53c5ad62a06288bbe0ba6281096bdec6d57e1bf5
krasyid822 <krasyid822@gmail.com>
2025-10-29 14:17:30 +0700

    Refactor UniversalWebView to use StatefulWidget for improved loading state management and error handling

--- lib/pages/webview_mobile.dart
+++ lib/pages/webview_mobile.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class UniversalWebView extends StatelessWidget {
+class UniversalWebView extends StatefulWidget {
   final String url;
 
   const UniversalWebView({super.key, required this.url});
 
   @override
-  Widget build(BuildContext context) {
-    final controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(url));
+  State<UniversalWebView> createState() => _UniversalWebViewState();
+}
 
-    return WebViewWidget(controller: controller);
+class _UniversalWebViewState extends State<UniversalWebView> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            setState(() {
+              _isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              _isLoading = false;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {
+            // You can handle errors here if you want
+            setState(() {
+              _isLoading = false;
+            });
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        WebViewWidget(controller: _controller),
+        if (_isLoading)
+          const Center(
+            child: CircularProgressIndicator(),
+          ),
+      ],
+    );
   }
 }
--- lib/pages/webview_web.dart
+++ lib/pages/webview_web.dart
@@ -2,25 +2,54 @@ import 'package:flutter/material.dart';
 import 'dart:ui_web' as ui_web;
 import 'dart:html';
 
-class UniversalWebView extends StatelessWidget {
+class UniversalWebView extends StatefulWidget {
   final String url;
 
   const UniversalWebView({super.key, required this.url});
 
   @override
-  Widget build(BuildContext context) {
+  State<UniversalWebView> createState() => _UniversalWebViewState();
+}
+
+class _UniversalWebViewState extends State<UniversalWebView> {
+  bool _isLoading = true;
+  late final String _viewType;
+
+  @override
+  void initState() {
+    super.initState();
     // Create a unique view factory name.
-    final String viewType = 'iframe-$url';
+    _viewType = 'iframe-${widget.url}';
 
     final IFrameElement iFrameElement = IFrameElement()
-      ..src = url
-      ..style.border = 'none';
+      ..src = widget.url
+      ..style.border = 'none'
+      ..onLoad.listen((event) {
+        if (mounted) {
+          setState(() {
+            _isLoading = false;
+          });
+        }
+      });
 
     ui_web.platformViewRegistry.registerViewFactory(
-      viewType,
+      _viewType,
       (int viewId) => iFrameElement,
     );
+  }
 
-    return HtmlElementView(viewType: viewType);
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        HtmlElementView(
+          viewType: _viewType,
+        ),
+        if (_isLoading)
+          const Center(
+            child: CircularProgressIndicator(),
+          ),
+      ],
+    );
   }
 }
